1121196f3b7e00f19f582fce24c5272ada1caf9c
ahmedhosam283843 <ahmed.hosam@coformatique.com>
2022-09-03 16:30:43 +0200

    Create File and Folder Structure

--- frontend_react/src/App.js
+++ frontend_react/src/App.js
@@ -1,9 +1,18 @@
-import React from "react";
+import React from 'react';
 
-const App = () => {
-    return (
-        <h1>App</h1>
-    );
-}
+import { About, Footer, Header, Skills, Testimonial, Work } from './container';
+import { Navbar } from './components';
+
+const App = () => (
+  <div className="app">
+    <Navbar />
+    <Header />
+    <About />
+    <Work />
+    <Skills />
+    <Testimonial />
+    <Footer />
+  </div>
+);
 
 export default App;
