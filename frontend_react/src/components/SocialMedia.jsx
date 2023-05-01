a0c0cb67f63a76196e6d82dd4388862dd2f909b4
ahmedhosam283843 <ahmed.hosam283843@gmail.com>
2023-05-01 07:08:35 +0300

    feat: update social media icons

--- frontend_react/src/components/SocialMedia.jsx
+++ frontend_react/src/components/SocialMedia.jsx
@@ -1,19 +1,17 @@
-import React from 'react';
-import { BsTwitter, BsInstagram } from 'react-icons/bs';
-import { FaFacebookF } from 'react-icons/fa';
-
+import React from "react";
+import{FaGithub, FaLinkedin} from "react-icons/fa"; 
 const SocialMedia = () => (
   <div className="app__social">
     <div>
-      <BsTwitter />
+      <a href="https://www.linkedin.com/in/ahmed-hosam28/" target="blank">
+        <FaLinkedin />
+      </a>
     </div>
     <div>
-      <FaFacebookF />
-    </div>
-    <div>
-      <BsInstagram />
+      <a href="https://github.com/ahmedhosam283843" target="blank">
+        <FaGithub />
+      </a>
     </div>
   </div>
 );
-
 export default SocialMedia;
