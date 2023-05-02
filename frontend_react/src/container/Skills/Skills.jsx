5fdbd8ad09c162f894ee7b6256c809ec23e20030
ahmedhosam283843 <ahmed.hosam283843@gmail.com>
2023-05-02 18:57:13 +0300

    fix: duplicate tooltips for same name experiences

--- frontend_react/src/container/Skills/Skills.jsx
+++ frontend_react/src/container/Skills/Skills.jsx
@@ -1,10 +1,10 @@
-import React, { useState, useEffect } from 'react';
-import { motion } from 'framer-motion';
-import ReactTooltip from 'react-tooltip';
+import React, { useState, useEffect } from "react";
+import { motion } from "framer-motion";
+import ReactTooltip from "react-tooltip";
 
-import { AppWrap, MotionWrap } from '../../wrapper';
-import { urlFor, client } from '../../client';
-import './Skills.scss';
+import { AppWrap, MotionWrap } from "../../wrapper";
+import { urlFor, client } from "../../client";
+import "./Skills.scss";
 
 const Skills = () => {
   const [experiences, setExperiences] = useState([]);
@@ -47,45 +47,45 @@ const Skills = () => {
           ))}
         </motion.div>
         <div className="app__skills-exp">
-          {
-          experiences.sort((a, b) => b.year - a.year).map((experience) => (
-            <motion.div
-              className="app__skills-exp-item"
-              key={experience.year}
-            >
-              <div className="app__skills-exp-year">
-                <p className="bold-text">{experience.year}</p>
-              </div>
-              <motion.div className="app__skills-exp-works">
-                {experience.works.map((work) => (
-                  <>
-                    <motion.div
-                      whileInView={{ opacity: [0, 1] }}
-                      transition={{ duration: 0.5 }}
-                      className="app__skills-exp-work"
-                      data-tip
-                      data-for={work.name}
-                      key={work.name}
-                    >
-                      <h4 className="bold-text">{work.name}</h4>
-                      <p className="p-text">{work.company}</p>
-                    </motion.div>
-                    <ReactTooltip
-                      id={work.name}
-                      effect="solid"
-                      arrowColor="#fff"
-                      place="top"
-                      className="skills-tooltip"
-                      offset={{ right: -160 }}
-                    >
-                      {work.desc}
-                    </ReactTooltip>
-                    
-                  </>
-                ))}
+          {experiences
+            .sort((a, b) => b.year - a.year)
+            .map((experience) => (
+              <motion.div
+                className="app__skills-exp-item"
+                key={experience.year}
+              >
+                <div className="app__skills-exp-year">
+                  <p className="bold-text">{experience.year}</p>
+                </div>
+                <motion.div className="app__skills-exp-works">
+                  {experience.works.map((work, index) => (
+                    <>
+                      <motion.div
+                        whileInView={{ opacity: [0, 1] }}
+                        transition={{ duration: 0.5 }}
+                        className="app__skills-exp-work"
+                        data-tip
+                        data-for={`tooltip-${experience.year}-${index}`}
+                        key={`${work.name}-${index}`}
+                      >
+                        <h4 className="bold-text">{work.name}</h4>
+                        <p className="p-text">{work.company}</p>
+                      </motion.div>
+                      <ReactTooltip
+                        id={`tooltip-${experience.year}-${index}`}
+                        effect="solid"
+                        arrowColor="#fff"
+                        place="top"
+                        className="skills-tooltip"
+                        offset={{ right: -160 }}
+                      >
+                        {work.desc}
+                      </ReactTooltip>
+                    </>
+                  ))}
+                </motion.div>
               </motion.div>
-            </motion.div>
-          ))}
+            ))}
         </div>
       </div>
     </>
@@ -93,7 +93,7 @@ const Skills = () => {
 };
 
 export default AppWrap(
-  MotionWrap(Skills, 'app__skills'),
-  'skills',
-  'app__whitebg',
+  MotionWrap(Skills, "app__skills"),
+  "skills",
+  "app__whitebg"
 );
