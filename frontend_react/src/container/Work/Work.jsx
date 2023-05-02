d5f6adab982eccc2734dd461fbb71f57fde5b05a
ahmedhosam283843 <ahmed.hosam283843@gmail.com>
2023-05-02 03:26:42 +0300

    feat: remove project demo icon

--- frontend_react/src/container/Work/Work.jsx
+++ frontend_react/src/container/Work/Work.jsx
@@ -69,7 +69,7 @@ const Work = () => {
                 transition={{ duration: 0.25, ease: 'easeInOut', staggerChildren: 0.5 }}
                 className="app__work-hover app__flex"
               >
-                <a href={work.projectLink} target="_blank" rel="noreferrer">
+                {/* <a href={work.projectLink} target="_blank" rel="noreferrer">
 
                   <motion.div
                     whileInView={{ scale: [0, 1] }}
@@ -79,7 +79,7 @@ const Work = () => {
                   >
                     <AiFillEye />
                   </motion.div>
-                </a>
+                </a> */}
                 <a href={work.codeLink} target="_blank" rel="noreferrer">
                   <motion.div
                     whileInView={{ scale: [0, 1] }}
